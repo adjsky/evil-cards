26687994407df5a8f53dd30c988bfdfba999d9e6
adjsky <igorlfmartins@mail.ru>
2022-10-12 21:07:04 +0300

    testing zod, ts-pattern and ws

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,49 @@
 import Fastify from "fastify"
 import websocketPlugin from "@fastify/websocket"
+import { nanoid } from "nanoid"
+import z from "zod"
+import { match } from "ts-pattern"
 
 const fastify = Fastify({ logger: true })
 await fastify.register(websocketPlugin)
 
-fastify.get("/", { websocket: true }, (connection) => {
-  connection.socket.on("message", () => {
-    connection.socket.send("hi from server")
+const sessions = new Map<string, any>()
+
+const MessageSchema = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("create-room"),
+    data: z.object({ name: z.string() })
+  }),
+  z.object({
+    type: z.literal("connect"),
+    data: z.object({ name: z.string() })
+  })
+])
+
+fastify.get("/", { websocket: true }, ({ socket }) => {
+  socket.on("message", (rawData) => {
+    try {
+      const message = MessageSchema.parse(JSON.parse(rawData.toString()))
+
+      match(message)
+        .with({ type: "create-room" }, ({ data }) => {
+          const roomId = nanoid(5)
+          sessions.set(roomId, { users: [data.name] })
+
+          console.log(sessions)
+        })
+        .with({ type: "connect" }, () => {
+          //
+        })
+        .exhaustive()
+    } catch (error) {
+      socket.send(
+        JSON.stringify({
+          type: "error",
+          data: error
+        })
+      )
+    }
   })
 })
 
