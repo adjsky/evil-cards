0639d52ae06524279b5c5f15cf45fd3afe1dbe0d
adjsky <igorlfmartins@mail.ru>
2022-11-08 01:47:34 +0300

    specify anonymous crossOrigin to prevent failing eslint check

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;700&display=swap"
