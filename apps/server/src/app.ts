90beaddb379a2821f10d47bfa5131b8cfe17842c
adjsky <igorlfmartins@mail.ru>
2022-12-04 14:02:49 +0300

    add memory logger

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -6,7 +6,21 @@ import memoryLogPlugin from "./plugins/log-memory"
 import websocketRoutes from "./routes/ws"
 import { env } from "./env"
 
-const fastify = Fastify({ logger: true })
+const envLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname"
+      }
+    }
+  },
+  production: true,
+  test: false
+}
+
+const fastify = Fastify({ logger: envLogger[env.NODE_ENV] })
 
 await fastify.register(memoryLogPlugin, { enabled: env.LOG_MEMORY })
 await fastify.register(websocketPlugin)
--- apps/server/src/plugins/log-memory.ts
+++ apps/server/src/plugins/log-memory.ts
@@ -1,32 +1,37 @@
-import type { FastifyPluginCallback } from "fastify"
+import { FastifyPluginCallback, FastifyBaseLogger } from "fastify"
 
-function logMemory() {
-  const formatMemoryUsage = (data: number) =>
-    `${Math.round((data / 1024 / 1024) * 100) / 100} MB`
+function getMemoryLogger(logger: FastifyBaseLogger) {
+  return () => {
+    const formatMemoryUsage = (data: number) =>
+      `${Math.round((data / 1024 / 1024) * 100) / 100} MB`
 
-  const memoryData = process.memoryUsage()
+    const memoryData = process.memoryUsage()
 
-  const memoryUsage = {
-    rss: `${formatMemoryUsage(
-      memoryData.rss
-    )} -> Resident Set Size - total memory allocated for the process execution`,
-    heapTotal: `${formatMemoryUsage(
-      memoryData.heapTotal
-    )} -> total size of the allocated heap`,
-    heapUsed: `${formatMemoryUsage(
-      memoryData.heapUsed
-    )} -> actual memory used during the execution`,
-    external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`
+    const memoryUsage = {
+      rss: `${formatMemoryUsage(
+        memoryData.rss
+      )} => Resident Set Size - total memory allocated for the process execution`,
+      heapTotal: `${formatMemoryUsage(
+        memoryData.heapTotal
+      )} => total size of the allocated heap`,
+      heapUsed: `${formatMemoryUsage(
+        memoryData.heapUsed
+      )} => actual memory used during the execution`,
+      external: `${formatMemoryUsage(memoryData.external)} -> external memory`
+    }
+
+    logger.info(memoryUsage)
   }
-
-  console.log(memoryUsage)
 }
 
 const memoryLogPlugin: FastifyPluginCallback<{
   intervalInSeconds?: number
   enabled?: boolean
-}> = (_, { intervalInSeconds = 10, enabled }, done) => {
+}> = (fastify, { intervalInSeconds = 30, enabled }, done) => {
+  const logMemory = getMemoryLogger(fastify.log)
+
   if (enabled) {
+    logMemory()
     setInterval(logMemory, intervalInSeconds * 1000)
   }
 
