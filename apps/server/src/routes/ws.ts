c283ed3ad38f550967b7512c91245e2685dfab65
adjsky <igorlfmartins@mail.ru>
2023-01-10 18:17:28 +0300

    fix ws path

--- apps/server/src/routes/ws.ts
+++ apps/server/src/routes/ws.ts
@@ -2,7 +2,7 @@ import game from "../game"
 import type { FastifyPluginCallback } from "fastify"
 
 const websocketRoutes: FastifyPluginCallback = (fastify, _, done) => {
-  fastify.get("/", { websocket: true }, ({ socket }) => {
+  fastify.get("/ws", { websocket: true }, ({ socket }) => {
     game.controller.handleConnection(socket)
   })
 
