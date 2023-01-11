e8e65571540191bd42d9399ed6aa2c23780c38e9
adjsky <igorlfmartins@mail.ru>
2023-01-11 23:22:35 +0300

    cache sounds

--- apps/client/src/audio/sounds.ts
+++ apps/client/src/audio/sounds.ts
@@ -1,21 +1,33 @@
 import type { Message as ReceiveMessage } from "@evil-cards/server/src/lib/ws/send"
+import type { Entries } from "type-fest"
 
 const sounds: Partial<Record<ReceiveMessage["type"], string>> = {
   gamestart: "/sounds/countdown.wav",
   userjoined: "/sounds/user-joined.wav",
   choose: "/sounds/flip-card.wav"
 }
+const cachedSounds: Partial<Record<ReceiveMessage["type"], HTMLAudioElement>> =
+  {}
 let preloadedSounds = false
 
 export function processMessageAndPlaySound(message: ReceiveMessage) {
+  const cachedSound = cachedSounds[message.type]
   const messageSound = sounds[message.type]
 
   if (!messageSound) {
     return
   }
 
-  const audio = new Audio(messageSound)
-  audio.play()
+  if (!cachedSound) {
+    const audio = new Audio(messageSound)
+    cachedSounds[message.type] = audio
+
+    audio.play()
+
+    return
+  }
+
+  cachedSound.play()
 }
 
 export function preloadSounds() {
@@ -23,8 +35,8 @@ export function preloadSounds() {
     return
   }
 
-  for (const sound of Object.values(sounds)) {
-    new Audio(sound)
+  for (const [key, value] of Object.entries(sounds) as Entries<typeof sounds>) {
+    cachedSounds[key] = new Audio(value)
   }
 
   preloadedSounds = true
