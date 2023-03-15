abcccfc95d4ba841fc628607c11a1496e465f553
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2023-03-16 00:34:18 +0300

    Update start-services.sh

--- deploy/scripts/start-services.sh
+++ deploy/scripts/start-services.sh
@@ -4,7 +4,6 @@ COMPOSE_PATH=$APP_PATH/$SOURCE_DIR/deploy/docker-compose.yml
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/swap-envs.sh
 
-docker-compose -f $COMPOSE_PATH pull
 docker-compose -f $COMPOSE_PATH up -d --remove-orphans
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/cleanup.sh
