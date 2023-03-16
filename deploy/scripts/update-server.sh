ce3d0814232a4e88d2f6efd246a10e8418d01d01
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2023-03-16 17:29:50 +0300

    add logs to update-server.sh

--- deploy/scripts/update-server.sh
+++ deploy/scripts/update-server.sh
@@ -2,6 +2,7 @@
 
 COMPOSE_PATH=$APP_PATH/$SOURCE_DIR/deploy/docker-compose.yml
 SERVERS_TO_START=2
+SECONDS_TO_REMOVE_CONTAINER=1800
 
 CURRENT_RUNNING_SERVERS=()
 
@@ -9,8 +10,10 @@ for CONTAINER in $(docker-compose -f $COMPOSE_PATH ps server | tail -n +3 | awk 
   CURRENT_RUNNING_SERVERS+=($CONTAINER)
 
   (
-    nohup sh -c "sleep 1800 && docker stop $CONTAINER && docker rm $CONTAINER" >/dev/null 2>&1
+    nohup sh -c "sleep $SECONDS_TO_REMOVE_CONTAINER && docker stop $CONTAINER && docker rm $CONTAINER" >/dev/null 2>&1
   ) &
+  
+  echo "$CONTAINER will be stopped and removed after $SECONDS_TO_REMOVE_CONTAINER seconds"
 done
 
 SCALE=$((4 + ${#CURRENT_RUNNING_SERVERS[@]} - SERVERS_TO_START))
@@ -29,5 +32,7 @@ for CONTAINER in $(docker-compose -f $COMPOSE_PATH ps server | tail -n +3 | awk 
 done
 
 docker-compose -f $COMPOSE_PATH exec -T redis redis-cli -n 0 SET servers "${AVAILABLE_SERVERS[*]}"
+
+echo "Updated servers in redis, ids: ${AVAILABLE_SERVERS[@]}"
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/cleanup.sh
