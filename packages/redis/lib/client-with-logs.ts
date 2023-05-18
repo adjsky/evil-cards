bd414c845a30c55bec5ad28c4b1652f3ee87b06f
adjsky <igorlfmartins@mail.ru>
2023-05-18 23:57:26 +0300

    client with logs finally??

--- packages/redis/lib/client-with-logs.ts
+++ packages/redis/lib/client-with-logs.ts
@@ -14,18 +14,53 @@ type CallableKeys<T> = keyof {
   [P in keyof T as T[P] extends () => unknown ? P : never]: unknown
 }
 
-type RedisKey = keyof RedisClientType
-type RedisCommand = CallableKeys<RedisClientType>
+type CallableSyncKeys<T> = keyof {
+  [P in keyof T as T[P] extends () => unknown
+    ? T[P] extends () => Promise<unknown>
+      ? never
+      : P
+    : never]: unknown
+}
 
-function isCommand(
+type CallableAsyncKeys<T> = keyof {
+  [P in keyof T as T[P] extends () => Promise<unknown> ? P : never]: unknown
+}
+
+type Command = CallableKeys<RedisClientType>
+type SyncCommand = CallableSyncKeys<RedisClientType>
+type AsyncCommand = CallableAsyncKeys<RedisClientType>
+
+const arrayOfAll =
+  <T>() =>
+  <U extends T[]>(array: U & ([T] extends [U[number]] ? unknown : "Invalid")) =>
+    array
+
+const arrayOfAllSyncCommands = arrayOfAll<SyncCommand>()
+
+const syncCommands = arrayOfAllSyncCommands([
+  "MULTI",
+  "duplicate",
+  "eventNames",
+  "getMaxListeners",
+  "multi",
+  "ref",
+  "removeAllListeners",
+  "scanIterator",
+  "unref"
+])
+
+function isAsyncCommand(
   redisClient: RedisClientType,
-  key: string | symbol
-): key is RedisCommand {
-  if (key == "withContext") {
-    return false
-  }
+  command: string | symbol
+): command is AsyncCommand {
+  return (
+    typeof redisClient[command as Command] == "function" &&
+    !isSyncCommand(command)
+  )
+}
 
-  return key in redisClient && typeof redisClient[key as RedisKey] == "function"
+function isSyncCommand(command: string | symbol): command is SyncCommand {
+  return syncCommands.findIndex((syncCommand) => syncCommand == command) != -1
 }
 
 export function getClientWithLogs(
@@ -36,13 +71,34 @@ export function getClientWithLogs(
 
   const originalDuplicate = redisClient.duplicate.bind(redisClient)
 
+  const handleGet = (
+    target: RedisClientType,
+    command: string | symbol,
+    receiver: unknown,
+    ctx?: ReqContext
+  ) => {
+    if (isSyncCommand(command)) {
+      return getSyncCommandWithLogs(
+        target,
+        command,
+        ctx ? logWithCtx(ctx, log) : log
+      )
+    }
+
+    if (isAsyncCommand(target, command)) {
+      return getAsyncCommandWithLogs(
+        target,
+        command,
+        ctx ? logWithCtx(ctx, log) : log
+      )
+    }
+
+    return Reflect.get(target, command, receiver)
+  }
+
   const proxy = new Proxy(redisClient, {
     get(target, command, receiver) {
-      if (!isCommand(target, command)) {
-        return Reflect.get(target, command, receiver)
-      }
-
-      return getCommandWithLogs(target, command, log)
+      return handleGet(target, command, receiver)
     }
   })
 
@@ -50,11 +106,7 @@ export function getClientWithLogs(
     withContext(ctx: ReqContext) {
       return new Proxy(redisClient, {
         get(target, command, receiver) {
-          if (!isCommand(target, command)) {
-            return Reflect.get(target, command, receiver)
-          }
-
-          return getCommandWithLogs(target, command, logWithCtx(ctx, log))
+          return handleGet(target, command, receiver, ctx)
         }
       })
     },
@@ -66,51 +118,74 @@ export function getClientWithLogs(
   })
 }
 
-function getCommandWithLogs(
+function getAsyncCommandWithLogs(
   target: RedisClientType,
-  command: RedisCommand,
+  command: AsyncCommand,
   log: Logger
 ) {
-  return (...args: unknown[]) => {
-    const logResult = (result: unknown) => {
-      log.info(
-        {
-          command,
-          args,
-          result
-        },
-        "finished redis command"
-      )
-    }
-
-    const logError = (error: unknown) => {
-      log.error(
-        {
-          err: error,
-          command,
-          args
-        },
-        "received a redis error"
-      )
-    }
-
+  return async (...args: unknown[]) => {
     try {
-      const result = Reflect.apply(target[command], target, args)
+      const result = await Reflect.apply(target[command], target, args)
 
-      if (result instanceof Promise) {
-        result.then(logResult).catch((error) => {
-          logError(error)
-          throw error
-        })
-      } else {
-        logResult(result)
-      }
+      logResult(log, command, result, args)
 
       return result
     } catch (error) {
-      logError(error)
+      logError(log, command, error, args)
 
       throw error
     }
   }
+}
+
+function getSyncCommandWithLogs(
+  target: RedisClientType,
+  command: SyncCommand,
+  log: Logger
+) {
+  return (...args: unknown[]) => {
+    try {
+      const result = Reflect.apply(target[command], target, args)
+
+      logResult(log, command, result, args)
+
+      return result
+    } catch (error) {
+      logError(log, command, error, args)
+
+      throw error
+    }
+  }
+}
+
+function logResult(
+  log: Logger,
+  command: Command,
+  result: unknown,
+  args: unknown[]
+) {
+  log.info(
+    {
+      command,
+      args,
+      result
+    },
+    "finished redis command"
+  )
+}
+
+function logError(
+  log: Logger,
+  command: Command,
+  error: unknown,
+  args: unknown[]
+) {
+  log.error(
+    {
+      err: error,
+      command,
+      args
+    },
+    "received a redis error"
+  )
 }
