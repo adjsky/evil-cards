7e66fc906885317aa30c59c12783bc80b4065568
adjsky <igorlfmartins@mail.ru>
2023-05-31 16:23:47 +0300

    pull on services start

--- deploy/scripts/start-services.sh
+++ deploy/scripts/start-services.sh
@@ -4,6 +4,7 @@ COMPOSE_PATH=$APP_PATH/$SOURCE_DIR/deploy/compose/docker-compose.$DEPLOY_ENV.yml
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/swap-envs.sh
 
+docker compose -f $COMPOSE_PATH pull
 docker compose -f $COMPOSE_PATH up -d --remove-orphans
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/cleanup.sh
