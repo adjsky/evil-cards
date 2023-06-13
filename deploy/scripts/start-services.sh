f688fe25d63f34380498dfadd5e7a459f1fcc7b8
adjsky <igorlfmartins@mail.ru>
2023-06-14 00:42:18 +0300

    test -d

--- deploy/scripts/start-services.sh
+++ deploy/scripts/start-services.sh
@@ -5,6 +5,6 @@ COMPOSE_PATH=$APP_PATH/$SOURCE_DIR/deploy/compose/docker-compose.$DEPLOY_ENV.yml
 $APP_PATH/$SOURCE_DIR/deploy/scripts/swap-envs.sh
 
 docker compose -f $COMPOSE_PATH pull
-docker compose -f $COMPOSE_PATH up --wait --remove-orphans --no-recreate
+docker compose -f $COMPOSE_PATH up -d --remove-orphans --no-recreate
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/cleanup.sh
--- deploy/scripts/update-client.sh
+++ deploy/scripts/update-client.sh
@@ -5,6 +5,6 @@ COMPOSE_PATH=$APP_PATH/$SOURCE_DIR/deploy/compose/docker-compose.$DEPLOY_ENV.yml
 $APP_PATH/$SOURCE_DIR/deploy/scripts/swap-envs.sh
 
 docker compose -f $COMPOSE_PATH pull client
-docker compose -f $COMPOSE_PATH up --wait --no-deps client
+docker compose -f $COMPOSE_PATH up -d --no-deps client
 
 $APP_PATH/$SOURCE_DIR/deploy/scripts/cleanup.sh
