df8d9094521c417dc0403a8bceb565be66287b45
adjsky <igorlfmartins@mail.ru>
2023-11-26 19:44:38 +0300

    add sentry

--- apps/client/env.ts
+++ apps/client/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 export default defineConfig({
   validator: "zod",
   schema: {
-    VITE_LOAD_BALANCER_PATH: z.string()
+    VITE_LOAD_BALANCER_PATH: z.string(),
+    VITE_SENTRY_DSN: z.string().optional()
   }
 })
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 
 import App from "./app"
 import "./globals.css"
+import "./sentry"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
