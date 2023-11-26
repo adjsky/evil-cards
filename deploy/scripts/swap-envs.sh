f04c486f27ad53282bf20876e9865e46051b5a3d
adjsky <igorlfmartins@mail.ru>
2023-11-26 18:20:05 +0300

    temp

--- deploy/scripts/swap-envs.sh
+++ deploy/scripts/swap-envs.sh
@@ -3,8 +3,8 @@
 SERVICES=("plausible" "postgres" "load-balancer" "server")
 
 for SERVICE in "${SERVICES[@]}"; do
-    SOURCE=$APP_PATH/$SOURCE_DIR/deploy/envs/$DEPLOY_ENV/$SERVICE.env
-    FILLED=$APP_PATH/envs/$SERVICE.env
+    SOURCE=$APP_PATH/$SOURCE_DIR/deploy/envs/$SERVICE.env
+    FILLED=$APP_PATH/envs/$DEPLOY_ENV/$SERVICE.env
 
     rm $SOURCE
     ln -s $FILLED $SOURCE
