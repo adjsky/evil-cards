1ee3976eb8700e2641f55c3546800ba6e2e68aa4
adjsky <igorlfmartins@mail.ru>
2023-12-02 21:40:44 +0300

    move rules svgs to assets

--- apps/client/src/components/rules.tsx
+++ apps/client/src/components/rules.tsx
@@ -1,32 +1,50 @@
 import React, { useState } from "react"
 
+import { ReactComponent as FirstRuleImage } from "@/assets/rules/1.svg"
+import { ReactComponent as SecondRuleImage } from "@/assets/rules/2.svg"
+import { ReactComponent as ThirdRuleImage } from "@/assets/rules/3.svg"
+import { ReactComponent as FourthRuleImage } from "@/assets/rules/4.svg"
+import { ReactComponent as FifthsRuleImage } from "@/assets/rules/5.svg"
+
 import styles from "./rules.module.css"
 
 const rules = [
-  "Всем игрокам раздаётся по 10 белых карт. На экране появляется красная карта",
-  "Игроки дополняют предложение своей белой картой",
-  "Ведущий зачитывает варианты и выбирает самую смешную карту",
-  "Побеждает самый смешной вариант по мнению ведущего",
-  "Игрок, заработавший 10 очков, побеждает!"
+  {
+    text: "Всем игрокам раздаётся по 10 белых карт. На экране появляется красная карта",
+    image: FirstRuleImage
+  },
+  {
+    text: "Игроки дополняют предложение своей белой картой",
+    image: SecondRuleImage
+  },
+  {
+    text: "Ведущий зачитывает варианты и выбирает самую смешную карту",
+    image: ThirdRuleImage
+  },
+  {
+    text: "Побеждает самый смешной вариант по мнению ведущего",
+    image: FourthRuleImage
+  },
+  {
+    text: "Игрок, заработавший 10 очков, побеждает!",
+    image: FifthsRuleImage
+  }
 ]
 
 const Rules: React.FC = () => {
   const [currentRuleIndex, setCurrentRuleIndex] = useState(0)
 
+  const RuleImage = rules[currentRuleIndex].image
+
   return (
     <>
       <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <div className="relative flex max-h-[210px] w-full max-w-[210px] flex-auto items-center justify-center">
-          <img
-            src={`/static/rules/${currentRuleIndex + 1}.svg`}
-            alt={`Правило ${currentRuleIndex + 1}`}
-            className="absolute h-full w-full object-contain text-transparent"
-            loading="lazy"
-            decoding="async"
-          />
+        <div className="relative w-full max-w-[210px] flex-auto">
+          <RuleImage width="100%" height="100%" className="absolute inset-0" />
         </div>
+
         <p className="text-center text-base font-bold uppercase text-gray-100 sm:text-xl">
-          {currentRuleIndex + 1}. {rules[currentRuleIndex]}
+          {currentRuleIndex + 1}. {rules[currentRuleIndex].text}
         </p>
       </div>
       <NextButtons
