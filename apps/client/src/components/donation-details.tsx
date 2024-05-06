71050e8015a0e11acfd770b7bb497b43b97f28cc
adjsky <igorlfmartins@mail.ru>
2024-05-06 16:10:12 +0300

    add btc and eth addresses

--- apps/client/src/components/donation-details.tsx
+++ apps/client/src/components/donation-details.tsx
@@ -18,6 +18,26 @@ const DonationDetails: React.FC = () => {
           </ExternalUnderlineLink>
         </li>
         <li>
+          BTC -{" "}
+          <ExternalUnderlineLink
+            href="https://blockchair.com/bitcoin/address/bc1qd09wxvqxdm8jz5hfnt4fnw5ykhhxqwasr3mu32"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Blockchair
+          </ExternalUnderlineLink>
+        </li>
+        <li>
+          ETH -{" "}
+          <ExternalUnderlineLink
+            href="https://blockchair.com/ethereum/address/0x4075c0bdbe6649119B5E5844Bcf957d25b8B1e69"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Blockchair
+          </ExternalUnderlineLink>
+        </li>
+        <li>
           BNB (BEP-20) -{" "}
           <ExternalUnderlineLink
             href="https://blockchair.com/bnb/address/0x80D58D656b1195C03e8C68af6Fd8Ab79973EC706"
